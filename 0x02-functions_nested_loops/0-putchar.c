97bccd04bc00fcf1bfafacd5ab311ab954e22fb1
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-09-13 21:40:30 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,12 @@
 
 int main(void)
 {
-char word[] = "_putchar";
+char word[9] = "_putchar";
 int i;
- for(i = 0; i<8; i++);
+for (i = 0; i<10; i++);
 {
 _putchar(word[i]);
 _putchar('\n');
-}
-_putchar(9); 
+} 
 return (0);
 }
