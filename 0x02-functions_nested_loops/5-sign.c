92ad67738222afeda029fb3990fbb5ef48fe27ab
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-09-14 14:48:07 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: positive, zero or negative integer
- * Return: 1 if it's positive, 0 if it's negative
+ * Return: 1 if it's positive, 0 if it's zero, -1 if it's negative
  */
 
 int print_sign(int n)
