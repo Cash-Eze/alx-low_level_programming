4d9fcecbf44dac27db8280d27bab1ca6d54a9c9a
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-09-27 03:06:24 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _memcpy - a function to copy n bytes 
+ * _memcpy - a function to copy n bytes
  * @src: a memory location
  * @dest: another memory location
- * @n: number of bytes 
+ * @n: number of bytes
  * Return: Always 0
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -12,7 +12,7 @@ char *start = dest;
 
 while (n--)
 {
-*dest = src;
+*dest = *src;
 dest++;
 src++;
 }
