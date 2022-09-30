4313f658f56328b6f76ae1741b7ed658a642ecf1
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-09-29 19:59:41 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - a function to print string in reverse
+ * @s: string to print
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_putchar(++s);
+_print_rev_recursion(s++);
+}
+else
+return(0);
+}
+
