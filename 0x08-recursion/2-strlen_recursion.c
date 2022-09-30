9f4fc61794d2283ae9ba4e0b60aef99183714f93
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-09-29 21:21:36 -0700

    2strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - a fubction to print length of string
+ * @s: string to print
+ * Return: Always 0
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s)
+{
+_putchar(1 + _strlen_recursion(s + 1));
+return (0);
+}
+}
