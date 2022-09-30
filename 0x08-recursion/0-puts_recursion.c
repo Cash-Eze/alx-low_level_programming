5c2a91acbadee45ef387d3d546a63c8c83b28ade
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-09-29 19:36:36 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - A function to print a string
+ * @s: string to print
+ * Return: Nothing
+ */
+
+void _puts_recursion()
+{
+if (char *s)
+{
+_putchar(*s);
+_put_recursion(s + 1);
+}
+else 
+{
+_putchar("\n");
+}
+}
+
