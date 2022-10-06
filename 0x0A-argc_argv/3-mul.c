39fd4af9c73b17ac2545e5952efaed03474d9cb5
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-10-05 20:33:50 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,14 +7,12 @@
  * @argc:  argument counter
  * @argv: argument value
  *
- * Return: 1
+ * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
 int num1, num2, result;
-int mult = num1 * num2;
-result = mult;
 
 if (argv != 3)
 {
@@ -26,7 +24,7 @@ else
 {
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 *num2;
+result = num1 * num2;
 printf("%d\n", result);
 return (0);
 }
