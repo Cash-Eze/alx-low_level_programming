c3ab48b3389ca992a13b457d50b2c928979cf98e
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-10-05 19:23:55 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(int argc, char *argv)
+{
+for (int i = 0; i <= argc; i++)
+{
+if (argv[])
+printf("%d\n",arg[i]);
+}
+}
