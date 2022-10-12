1674a74cd3a441034cb5a3d08cb21446e1b91f05
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-10-11 21:27:57 -0700

    2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,5 +15,5 @@ d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
 
-printf("Name: %s\nAge: %0.1f\nOwner: %s\n", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
