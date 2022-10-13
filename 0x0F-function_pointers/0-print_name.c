d4ed8d279b716bc6cb820a15ca9a922273b62d17
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-10-13 08:21:26 -0700

    0-print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "MAIN.H"
+#include <stdio.h>
+
+/**
+ * print_name - a function to print a name
+ * @name: name function that contains name
+ * Return: Nothing
+ */
+
+void print_name(char *name,void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
