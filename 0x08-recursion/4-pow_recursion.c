96e9daf4230544dee951bf103593bc168bb00819
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-11-15 17:42:43 -0800

    raise to pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <math.h>
 
 /**
  * pow_recursion - function to raise to power
@@ -11,13 +10,13 @@
 
 int _pow_recursion(int x, int y)
 {
-if (y < 0)
+if (y == 0)
 {
 return (-1);
 }
-if (y >= 1)
+if (y == 1)
 {
-return (1);
+return (x);
 }
-return (pow(x, y));
+return (x *= _pow_recursion(x, y - 1));
 }
