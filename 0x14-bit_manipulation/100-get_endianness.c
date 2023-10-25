8cd856b5a8ceb5bf5831b0ceec7827f71c254c85
Cash-Eze <amoshiamatsu@gmail.com>
2023-10-24 19:24:03 -0700

    crack me

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,8 @@
 /**
- *  * get_endianness - Checks endianness of processor
- *   *
- *    * Return: 0 if cpu is beg endian, 1 if little endian
- *     */
+ ** get_endianness - Checks endianness of processor
+ **
+ ** Return: 0 if cpu is beg endian, 1 if little endian
+ **/
 int get_endianness(void)
 {
 unsigned int i = 1, *p;
